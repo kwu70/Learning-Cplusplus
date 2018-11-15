dc018c37cfb5bc6236295e33ff0bbbc76cbaf8a3
kwu70 <kwu70gt@gmail.com>
2018-11-14 19:28:21 -0500

    finished, but need to test edge cases

--- Doubly Linked List/DoublyLinkedList.cpp
+++ Doubly Linked List/DoublyLinkedList.cpp
@@ -60,8 +60,8 @@ class Double_LList {
         T* removeFirst();
         T* removeLast();
         T* removeAt(int pos);
-        void interator();
         bool exist(T* data);
+        void printAll();
         int size();
 
         //LIFO ~ push: add to stack, pop: remove from stack
@@ -133,38 +133,107 @@ void Double_LList<T>::insertAt(T* node, int pos) {
         cout << "Err: Range out of bound." << endl;
     }
     else {
-        for (int i = 0; i <= pos; i++, temp = temp->next) {
-            node->prev = temp->prev;
-            node->next = temp;
-            temp->prev->next = node;
-            temp->prev = node;
+        for (int i = 0; i < pos; i++) {
+            temp = temp->next;
         }
+        node->prev = temp->prev;
+        node->next = temp;
+        temp->prev->next = node;
+        temp->prev = node;
     }
 }
 
 template <class T>
 T* Double_LList<T>::removeFirst() {
+    T* data;
 
+    if (this->ll_size == 0) {
+        return NULL;
+    }
+    else if (this->ll_size == 1) {
+        data = this->head->getData();
+        this->head = NULL;
+        this->tail = NULL;
+        this->ll_size = 0;
+    }
+    else {
+        data = this->head->getData();
+        this->head->next->prev = NULL;
+        this->head = this->head->next;
+        this->ll_size--;
+    }
+    return data;
 }
 
 template <class T>
 T* Double_LList<T>::removeLast() {
+    T* data;
 
+    if (this->ll_size == 0) {
+        return NULL;
+    }
+    else if (this->ll_size == 1) {
+        data = this->tail->getData();
+        this->head = NULL;
+        this->tail = NULL;
+        this->ll_size = 0;
+    }
+    else {
+        data = this->tail->getData();
+        this->tail->prev->next = NULL;
+        this->tail = this->tail->prev;
+        this->ll_size--;
+    }
+    return data;
 }
 
 template <class T>
 T* Double_LList<T>::removeAt(int pos) {
+    T* temp = this->head;
+    T* data;
 
-}
-
-template <class T>
-void Double_LList<T>::interator() {
-
+    if (pos == 0) {
+        data = this->removeFirst();
+    }
+    else if (pos == this->ll_size) {
+        data = this->removeLast();
+    }
+    else if (pos > this->ll_size){
+        cout << "Err: Range out of bound." << endl;
+        return NULL;
+    }
+    else {
+        for (int i = 0; i < pos; i++) {
+            temp = temp->next;
+        }
+        data = temp->getData();
+        temp->prev->next = temp->next;
+        temp->next->prev = temp->prev;
+        this->ll_size--;
+    }
+    return data;
 }
 
 template <class T>
 bool Double_LList<T>::exist(T* data) {
+    T* temp = this->head;
+    for (int i = 0; i < this->ll_size; i++) {
+        if (temp->getData() == data) {
+            break;
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
 
+template <class T>
+void Double_LList<T>::printAll() {
+    T* temp = this->head;
+    for (int i = 0; i < this->ll_size; i++) {
+        cout << temp->getData() << endl;
+        temp = temp->next;
+    }
 }
 
 template <class T>
